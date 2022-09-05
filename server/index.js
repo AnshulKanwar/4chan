da7243b301d9a18250bbdd1d5829b73131df562e
Anshul Kanwar <4n5hu1@protonmail.com>
2022-09-05 15:22:33 +0530

    Added bodyparser.json() middleware

--- server/index.js
+++ server/index.js
@@ -7,12 +7,13 @@ import postRoutes from "./routes/posts.js";
 const PORT = 5500;
 
 const app = express();
+app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb://localhost:27017";
+const CONNECTION_URL = "mongodb://localhost:27017/4chan";
 mongoose.connect(CONNECTION_URL);
 
 app.listen(PORT, () => {
